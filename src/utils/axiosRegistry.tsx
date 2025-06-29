4ea49e2d62a36d4b9cf16cef1985e6aeb8e13ceb
KerbsD <boijablo555@gmail.com>
2025-06-29 10:27:30 +0800

    Created a copy of axios config with proper interceptors

--- src/utils/axiosRegistry.tsx
+++ src/utils/axiosRegistry.tsx
@@ -1,31 +1,46 @@
-import axios, { AxiosRequestConfig, Method, AxiosError } from "axios";
-import cookies from "./cookies";
-import { API_URL } from "@/configs/environments";
+import axios, { AxiosRequestConfig, Method, AxiosInstance } from "axios";
 import { SESSION_COOKIE } from "@/configs/constants";
+import { API_URL } from "@/configs/environments";
+import cookies from "./cookies";
 
-const getAuthToken = () => {
-  return cookies.get(SESSION_COOKIE);
-};
-
-const axiosInstance = axios.create({
+const axiosConfig: AxiosInstance = axios.create({
   baseURL: API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
 });
 
-export const fetcher = async <D = never,>(
+axiosConfig.interceptors.request.use(
+  (config) => {
+    const token = cookies.get(SESSION_COOKIE)?.value;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+axiosConfig.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 403) {
+      console.warn("Unauthorized. Redirecting to login...");
+    }
+    return Promise.reject(error);
+  }
+);
+
+export const fetcher = <D = never>(
   method: Method,
   url: string,
   params?: object,
   options?: AxiosRequestConfig
-): Promise<D> => {
-  const token = getAuthToken();
-
+) => {
   const config: AxiosRequestConfig = {
     url,
     method,
-    headers: {
-      Authorization: token ? `Bearer ${token}` : "",
-      ...(options?.headers || {}),
-    },
     ...options,
   };
 
@@ -35,16 +50,5 @@ export const fetcher = async <D = never,>(
     config.data = params;
   }
 
-  try {
-    const response = await axiosInstance.request<D>(config);
-    return response.data;
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      if (error.response?.status === 403) {
-        console.error("Access denied: 403");
-      }
-    }
-
-    throw error;
-  }
+  return axiosConfig.request<D>(config).then((res) => res.data);
 };
